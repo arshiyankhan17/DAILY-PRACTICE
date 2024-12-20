d0d05b899157cf4fd43fbabed4e0f78ef1eef493
arshiyankhan17 <arshiyanahmedkhan17@gmail.com>
2024-12-20 14:14:39 +0500

    email & passward

---
+++ if&else.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+void main() {
+print("Enter  Email");
+var Email = numparse(stdin.readLineSync()!);
+print("Enter passward");
+var passward =num.parse(stdin.readLineSync()!);
+
+if (Email == 'username@gmail.com'&& passward == 123456  ) {
+if (Email =='username@gmail.com'&& passward == 123456 ) {
+  print("login is succsesfull ");
+} else if (passward == 123456 ) {
+  print("passward is correct ");
+
+} else {
+  print("Email is correct ");
+}
+} else  {
+  print("not correct Email or passward");
+}
+
+}
