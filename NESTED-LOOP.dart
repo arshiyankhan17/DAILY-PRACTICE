4372e08940769286783d4c4793cbda1e9e3c24fa
arshiyankhan17 <arshiyanahmedkhan17@gmail.com>
2024-12-26 13:29:18 +0500

    NASTED-FOR-LOOP

---
+++ NESTED-LOOP.dart
@@ -0,0 +1,16 @@
+void main (){
+List name = ["abdullah", "raza", "kashan","noman", "ali", "fardeen","ali", "arshiyan","imran","adnan"];
+for (var i = 1; i < 5; i++) {
+  for (var j = 1;j < 10;j++) {
+    print(" $i : $j");
+    print(name);
+ }
+   print("====================");
+}
+
+
+
+
+
+
+}
