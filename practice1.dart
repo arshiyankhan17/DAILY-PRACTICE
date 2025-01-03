208829680dc9b594713e77a4de420da31c347872
arshiyankhan17 <arshiyanahmedkhan17@gmail.com>
2025-01-03 14:34:20 +0500

    sum of two condition

---
+++ practice1.dart
@@ -0,0 +1,16 @@
+sumoftwocondition (){
+
+num number1  =88820;
+num number2  = 99920;
+
+num  abc = (number1 + number2 - number1);  
+
+print(abc);
+
+}
+
+void main (){
+
+sumoftwocondition();
+
+}
