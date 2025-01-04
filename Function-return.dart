e1df8e7f2e403af1e84875a1b45c233b40b21ad0
arshiyankhan17 <arshiyanahmedkhan17@gmail.com>
2025-01-04 16:06:28 +0500

    multipication of three condition & return

---
+++ Function-return.dart
@@ -0,0 +1,20 @@
+void main (){
+
+multiplycationofthreevalues ();
+
+int abc = multiplycationofthreevalues ();
+
+print(abc);
+
+}
+int  multiplycationofthreevalues (){
+
+int number1 = 88;
+int number2 = 70;
+int number3 = 140;
+
+int result = (number3 - number1 * number2    );
+// print(result);
+return  result;
+
+}
