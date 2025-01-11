30c135ff3be9a68276d76404623debd916a5970b
arshiyankhan17 <arshiyanahmedkhan17@gmail.com>
2025-01-11 15:33:37 +0500

    funcation fail & pass

---
+++ dartpractice.dart
@@ -0,0 +1,30 @@
+void main () {
+
+String name = 'arshiyan =';
+print(name);
+ studentInfo (String);
+}
+  studentInfo (String){
+
+int subject1 = 88;
+int subject2 = 59;
+int  subject3= 78;
+int  subject4= 84;
+int  subject5= 65;
+int  subject6= 77;
+int  totalmarks = 600;  
+num obtainmarks = (subject1 + subject2 + subject3 + subject4 + subject5 + subject6 );
+
+num result = (obtainmarks/totalmarks)*100;
+
+if (result <= 50 ) {
+  print('fail hogiya');
+} else {
+  print(' pass hogiya');
+}
+
+}
+
+
+
+
